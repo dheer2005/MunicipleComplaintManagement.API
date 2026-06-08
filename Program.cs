fd81f773a58c6f4d65e0370f992e5882eb7ab746
Dhirendra Yadav <dhirendranyadav@gmail.com>
2026-06-08 16:49:21 +0530

    Latest Db update

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAll",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200","https://complaintmgmt.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
